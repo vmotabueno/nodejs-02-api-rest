c1d14a566c1ad32d46b71e4686819a004d6d7fe4
Victor Mota Bueno <victor.bueno@precato.com.br>
2025-07-24 12:57:40 -0300

    Realizando queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+  return transactions
 })
 
 app
