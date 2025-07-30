58d5ef043222032b6365904c18db9f0a5858ec85
Victor Mota Bueno <victor.bueno@precato.com.br>
2025-07-30 09:28:06 -0300

    Criação de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,26 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import crypto from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/transactions', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+    const transaction = {
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    }
+
+    await knex('transactions').insert(transaction)
+
+    return reply.status(201).send()
   })
 }
