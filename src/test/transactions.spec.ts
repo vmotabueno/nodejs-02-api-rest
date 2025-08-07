cab1d9a216066f3897674efe0cb45481b8f862bd
Victor Mota Bueno <victor.bueno@precato.com.br>
2025-08-07 11:03:07 -0300

    testando listagem de transações

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { beforeAll, afterAll, describe, it } from 'vitest';
+import { beforeAll, expect, afterAll, describe, it } from 'vitest';
 import request from 'supertest';
 import { app } from '../app'; // corrigido
 
@@ -20,5 +20,27 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201);
+  });
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200);
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ]);
   });
 });
